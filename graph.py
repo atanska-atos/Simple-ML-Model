47a98229a77f3cfdbaff85a41a734c7e98fdbe96
atanska-atos <aleksandra.tanska8422@gmail.com>
2023-04-03 16:33:40 +0200

    Add pyplot graph

--- graph.py
+++ graph.py
@@ -1,9 +1,3 @@
-import graphing
+import matplotlib.pyplot as plt
 from train_model import *
 
-
-def create_graph(dataset, fitted_model):
-    graphing.scatter_2D(dataset, label_x="harness_size",
-                        label_y="boot_size",
-                        trendline=lambda x: fitted_model.params[1] * x + fitted_model.params[0]
-                        )
